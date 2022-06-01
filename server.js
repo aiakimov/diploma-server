b8cc93f8e559a53b32baf899a21ca2b9d9a31d76
aiakimov <akim.boroda@gmail.com>
2022-06-01 14:27:08 +0300

    init new server

---
+++ server.js
@@ -0,0 +1,17 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+server.listen(666, () => {
+  console.log("JSON Server is running");
+});
+server.use(jsonServer.bodyParser);
+server.use((req, res, next) => {
+  if (req.method === "POST") {
+    req.body.createdAt = Date.now();
+  }
+  next();
+});
